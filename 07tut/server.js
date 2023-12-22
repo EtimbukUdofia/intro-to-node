57fb5b9ad9cef6e98ff4b89462ed33e3e415e7bb
EtimbukUdofia <etimbukudofia9@gmail.com>
2023-12-22 14:21:26 +0100

    add cors, whitelist and an errorhandler

--- 07tut/server.js
+++ 07tut/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 3500;
 
 // a custom logger
 app.use(logger);
+
+// Cross Origin Resource Sharing
+const whiteList = ['http://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+  origin: (origin, callback) => {
+
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other words, form data:
@@ -64,6 +81,8 @@ app.get('/chain(.html)?', [one, two, three]);
 app.get('/*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 })
+
+app.use(errorHandler);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
